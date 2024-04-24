c3d6108ac0dd324fa2c43851359ebd14542b63da
Kevin <vincitbussiness@gmail.com>
2024-04-24 02:33:00 -0600

    Proyecto creado con endpoint que da la lista de los usuarios en la base de datos

---
+++ app.js
@@ -0,0 +1,37 @@
+const { createClient } = require("@libsql/client");
+require('dotenv').config();
+
+
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 3000
+const URL_DB = process.env.TURSO_DATABASE_URL
+const AUTH_DB_TOKEN = process.env.TURSO_AUTH_TOKEN
+
+const client = createClient({
+    url: URL_DB,
+    authToken: AUTH_DB_TOKEN
+});
+
+const data = async () => {
+    try {
+        const rows = await client.execute("SELECT * FROM users")
+        return rows.rows
+    } catch (error) {
+        console.log("Error en la recuperacion de datos ", error)
+        return []
+    }
+}
+
+app.get('/', async (req, res) => {
+    try {
+        const userData = await data()
+        res.json(userData)
+    } catch (error) {
+        res.status(500).json({ error: "Error obteniendo datos de usuario" })
+    }
+})
+
+app.listen(PORT, () => {
+    console.log('Servidor activo en el puertooooo ', PORT)
+})
